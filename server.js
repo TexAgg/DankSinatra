6f1a1cc8b6c2f4dfd037a2b97ad3d296e6f026ab
Matt G <matt.gaikema@tamu.edu>
2016-02-26 12:13:17 -0600

    Server modifications

--- server.js
+++ server.js
@@ -2,16 +2,20 @@
 
 const INFO = require('./info.js');
 const login = require("facebook-chat-api");
-//const http = require('http');
+const http = require('http');
 //const Firebase = require("firebase");
 
-/*
 http.createServer(function (request, response) {
   console.log("ping");
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end("");
 }).listen(process.env.PORT || 5000);
-*/
+
+setInterval(function() {
+  http.get("danksinatra.herokuapp.com", function(response) {
+    console.log("pong");
+  });
+}, 1800000 * Math.random() + 1200000); // between 20 and 50 min
 
 // Replace INFO.EMAIL and INFO.PASSWORD with the account email and password
 login({email: INFO.EMAIL, password: INFO.PASSWORD}, function callback (error, api) {
