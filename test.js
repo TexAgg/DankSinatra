4200d77264c5ebd44338956fed5b3a801a8c469f
Matt G <matt.gaikema@tamu.edu>
2016-03-02 17:23:24 -0600

    Documentation

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 //https://www.dropbox.com/home/Programming/Node/Hello?preview=main.js
 // https://teamtreehouse.com/matthewgaikema.json
+// http://www.wunderground.com/weather/api/d/docs?MR=1
 var request = http.get('http://api.wunderground.com/api/ecc2911f5f7c6247/conditions/q/CA/San_Francisco.json',function(response){
 	console.log(response.statusCode);
 	var body = '';
