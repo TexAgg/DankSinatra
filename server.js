cdd12ba751792bf8a9dfd478652e7582ac9b9a1d
Matt G <matt.gaikema@tamu.edu>
2016-03-21 14:15:14 -0500

    Fuck appstate

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ setInterval(function() {
  * Login via app-state to minimize Facebook reports
  * https://github.com/Schmavery/facebook-chat-api/blob/master/DOCS.md#logincredentials-options-callback
  */
-//login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, function callback (error, api) {
+//login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, {forceLogin: true}, function callback (error, api) {
 login({email: process.env.EMAIL, password: process.env.PASSWORD}, {forceLogin: true}, function callback (error, api) {
 	if(error) return console.error(error);
 	// Set appstate
